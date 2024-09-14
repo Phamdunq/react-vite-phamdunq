c9677d8436c9c02ec5128f40bb6070b02dda2a05
Pham dunq <phamdunq02122003@gmai.com>
2024-09-14 11:00:35 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,25 @@ import './style.css'
 //JSX : 1 parent
 //fragment: <> </>
 const MyComponent = () => {
+
+    //kiểu dữ liệu nguyên thủy
+    // const string = "Data" // string
+    // const number = 22 // number
+    // const bool = true // bool
+    // const strong = undefined //undefined
+    // const nulls = null // null  
+
+    //kiểu dữ liệu tham chiếu
+    // const array = [1, 2, 3]
+    const object = {
+        name: "eric",
+        age: 22
+    }
+
     return (
         <>
-            <div>Hello word</div>
+
+            <div> {JSON.stringify(object)} & Hello word</div>
             <div className="child">Child</div>
         </>
     )
