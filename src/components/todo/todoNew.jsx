6e4cd1d145ba2cbaf69919ea1081d7ae60b80255
Pham dunq <phamdunq02122003@gmai.com>
2024-09-26 00:39:30 +0700

    video 36

--- src/components/todo/todoNew.jsx
+++ src/components/todo/todoNew.jsx
@@ -1,11 +1,22 @@
 
 const TodoNew = (props) => {
     const {addNewTodo} = props
-    // addNewTodo("Pham dung")
+    // addNewTodo("Pham dung")// fire
+
+    const handelOnClick = () => {
+        alert('Click me')
+    }
+
+    const handelOnChange = (name) => {
+        console.log('>>> handelOnChange', name)
+    }
+
     return (
         <div className="todo-new">
-            <input type="text"/>
-            <button>Add</button>
+            <input onChange={(event) => {handelOnChange(event.target.value)}} type="text"/>
+            <button 
+                onClick={handelOnClick}
+            >Add</button>
         </div>
     )
 }
