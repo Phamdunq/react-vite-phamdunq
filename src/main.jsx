67b879f92cac99782f792386930bcf2492697670
Pham dunq <phamdunq02122003@gmai.com>
2024-09-26 14:24:02 +0700

    video 51

--- src/main.jsx
+++ src/main.jsx
@@ -11,12 +11,17 @@ import RegisterPage from './pages/register.jsx'
 import UserPage from './pages/user.jsx'
 import ProductsPage from './pages/products.jsx'
 import './styles/global.css'
+import TodoApp from './components/todo/todoApp.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
+      {
+        index: true,
+        element: <TodoApp />
+      },
       {
         path: "/users",
         element: <UserPage />
@@ -41,10 +46,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
 )
