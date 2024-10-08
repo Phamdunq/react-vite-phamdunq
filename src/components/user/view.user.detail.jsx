35b01e838579515fe2244b0b2cba966714183d78
Pham dunq <phamdunq02122003@gmai.com>
2024-10-08 09:58:08 +0700

    video 83

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,12 +1,31 @@
 import { Button, Drawer } from "antd";
+import { useState } from "react";
 
 const ViewUserDetail = (props) => {
   const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
 
+  const [selectedFile, setSelectedFile] = useState();
+  const [preview, setPreview] = useState();
+
+  const handelUploadFile = (event) => {
+    if (!event.target.files || event.target.files.length === 0) {
+      setSelectedFile(null);
+      setPreview(null);
+      return;
+    }
+
+    // I've kept this example simple by using the first image instead of multiple
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      setPreview(URL.createObjectURL(file));
+    }
+  };
+
   return (
     <>
       <Drawer
-      width={"40vw"}
+        width={"40vw"}
         title="Basic Drawer"
         onClose={() => {
           setDataDetail(null);
@@ -17,36 +36,72 @@ const ViewUserDetail = (props) => {
         {dataDetail ? (
           <>
             <p>id: {dataDetail._id}</p>
-            <br />  
+            <br />
             <p>name: {dataDetail.fullName}</p>
             <br />
             <p>email: {dataDetail.email}</p>
             <br />
             <p>phone: {dataDetail.phone}</p>
             <br />
-            <div>
-              <img 
-              height={150} width={200}
-              src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`}
+            <div
+              style={{
+                marginTop: "10px",
+                height: "100px",
+                width: "150px",
+                border: "1px solid #ccc",
+              }}
+            >
+              <img
+                style={{ height: "100%", width: "100%", objectFit: "contain" }}
+                src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${
+                  dataDetail.avatar
+                }`}
               />
             </div>
             <div>
-              <label 
-              style={{
-                display: "block",
-                width: "fit-content",
-                marginTop: "15px",
-                padding: "5px 10px",
-                background: "blue",
-                borderRadius: "5px",
-                cursor: "pointers"
-              }}
-              htmlFor="btnUpload"
+              <label
+                style={{
+                  display: "block",
+                  width: "fit-content",
+                  marginTop: "15px",
+                  padding: "5px 10px",
+                  background: "blue",
+                  borderRadius: "5px",
+                  cursor: "pointers",
+                }}
+                htmlFor="btnUpload"
               >
-                    Upload Avatar
+                Upload Avatar
               </label>
-              <input type="file" id="btnUpload" hidden/>
+              <input
+                type="file"
+                id="btnUpload"
+                hidden
+                //onChange={handelUploadFile}
+                onChange={(event) => {
+                  handelUploadFile(event);
+                }}
+              />
             </div>
+            {preview && (
+              <div
+                style={{
+                  marginTop: "10px",
+                  height: "100px",
+                  width: "150px",
+                  border: "1px solid #ccc",
+                }}
+              >
+                <img
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    objectFit: "contain",
+                  }}
+                  src={preview}
+                />
+              </div>
+            )}
           </>
         ) : (
           <p>Khong co du lieu</p>
