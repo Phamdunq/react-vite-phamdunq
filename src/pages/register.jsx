4461fc6c2e0e66825cf75595b7a1bd8684645f29
Pham dunq <phamdunq02122003@gmai.com>
2024-10-08 21:50:07 +0700

    video 89

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,8 +1,85 @@
+import { Button, Checkbox, Form, Input } from "antd";
 
 const RegisterPage = () => {
-    return (
-        <div>Register page</div>
-    )
-}
+  const [form] = Form.useForm();
 
-export default RegisterPage
+  const onFinish = (value) => {
+    console.log("check value:", value)
+  }
+
+
+
+  return (
+    <Form
+    form={form}
+      layout="vertical"
+      style={{ margin: "50px" }}
+        onFinish={onFinish}
+      //   onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="fullName"
+        name="fullName"
+        // rules={[
+        //   {
+        //     required: true,
+        //     message: "Please input your username!",
+        //   },
+        // ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Email"
+        name="email"
+        // rules={[
+        //   {
+        //     required: true,
+        //     message: "Please input your username!",
+        //   },
+        // ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        // rules={[
+        //   {
+        //     required: true,
+        //     message: "Please input your username!",
+        //   },
+        // ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        label="phone Number"
+        name="phone"
+        // rules={[
+        //   {
+        //     required: true,
+        //     message: "Please input your username!",
+        //   },
+        // ]}
+      >
+        <Input />
+      </Form.Item>
+
+
+      <Button
+        type="primary"
+        onClick={() => {
+          form.submit()
+        }}
+      >
+        Register
+      </Button>
+    </Form>
+  );
+};
+
+export default RegisterPage;
