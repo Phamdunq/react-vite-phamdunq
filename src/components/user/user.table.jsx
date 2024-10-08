67ad29ba7e7f1951f173ebc577f1dfa6d9d5e6ab
Pham dunq <phamdunq02122003@gmai.com>
2024-10-08 11:20:20 +0700

    video 85

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -31,6 +31,16 @@ const UserTable = (props) => {
 
   const columns = [
     {
+      title: "STT",
+      render: (_, record, index) => {
+        return (
+          <>
+          {index + 1}
+          </>
+        );
+      },
+    },
+    {
       title: "Id",
       dataIndex: "_id",
       render: (_, record) => {
